1cf31b56aa76923039f58c6b95624473ce03e1e0
georgemac510 <georgemac510@gmail.com>
2023-10-18 20:23:01 -0400

    Update contract and deploy to Scroll

--- hardhat-contracts/config.ts
+++ hardhat-contracts/config.ts
@@ -1,3 +1,3 @@
 
-    export const deReportAddress = "undefined"
+    export const stakingAddress = "0xdb7858A50e90233545309F7312cFBe92d7b795B5"
     
--- hardhat-contracts/scripts/deploy.ts
+++ hardhat-contracts/scripts/deploy.ts
@@ -3,19 +3,17 @@ const hre = require("hardhat");
 const fs = require('fs');
 
 async function main() {
-    const initialOwnerAddress = "0xdc3DE060FF06df185A780124E45a0115f739aFb9";
-
     const DeReport = await hre.ethers.getContractFactory("DeReport");
     console.log("Deploying DeReport...");
-    const deReport = await DeReport.deploy(initialOwnerAddress);
+    const deReport = await DeReport.deploy();
 
-    await deReport.waitForDeployment();
+    await deReport.deployed();
 
     console.log("DeReport contract deployed to:", deReport.address);
 
     // Add more debugging information
     console.log("Transaction hash:", deReport.deployTransaction.hash);
-    console.log("Gas used for deployment:", deReport.deployTransaction.gasUsed.toString());
+    // console.log("Gas used for deployment:", deReport.deployTransaction.gasUsed.toString());
 
     fs.writeFileSync('./config.ts', `
     export const stakingAddress = "${deReport.address}"
@@ -27,8 +25,3 @@ main().catch((error) => {
     process.exitCode = 1;
 });
 
-
-main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-});
